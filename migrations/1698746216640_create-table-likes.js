046f921f23b998c8a751824a543432cac322b23d
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2023-10-31 17:00:01 +0700

    add likes migration

---
+++ migrations/1698746216640_create-table-likes.js
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'cascade',
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"comments"',
+      onDelete: 'cascade',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('likes');
+};
